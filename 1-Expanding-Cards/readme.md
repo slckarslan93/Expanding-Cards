020a290bf8a29fb4d3e46e1e03fbc24f338172d4
Selcuk ARSLAN <45368072+slckarslan93@users.noreply.github.com>
2022-04-12 14:28:52 +0300

    Add files via upload

---
+++ 1-Expanding-Cards/readme.md
@@ -0,0 +1,12 @@
+#Expanding Cards
+
+<hr>
+<li>This project used Html, Css and javascript.
+<br>
+<li>
+panels are formatted to have a background image and a title.
+<hr>
+
+![İmage](/img/asdf.JPG)
+
+![İmage](/img/asdf2.JPG)
---
+++ 1-Expanding-Cards/script.js
@@ -0,0 +1,14 @@
+const panels = document.querySelectorAll('.panel');
+
+panels.forEach((panel)=>{
+  panel.addEventListener('click',()=>{
+    remoActiveClasses()
+    panel.classList.add('active')
+  })
+})
+
+function remoActiveClasses(){
+  panels.forEach(panel =>{
+    panel.classList.remove('active')
+  })
+}
